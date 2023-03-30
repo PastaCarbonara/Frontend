8ab1d1945ae2e41d97b7790045a58171becc38d2
Marisa <60745066+07Marisa99@users.noreply.github.com>
2023-03-30 11:00:41 +0200

    added an end line rule to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    extends: ['@react-native-community', "eslint-config-prettier"],
+    extends: ['@react-native-community', 'eslint-config-prettier'],
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+    },
 };
