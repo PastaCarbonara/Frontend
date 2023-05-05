59cb63bb4125a8a894456777530108fc63c9c1cc
mitchellvdhut <mitchellvdhut@gmail.com>
2023-05-05 16:20:41 +0200

    loading state for homescreen

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -14,44 +14,53 @@ const AuthContext = React.createContext<AuthContextType>({} as AuthContextType);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [authData, setAuthData] = useState<{
-        user_uuid: string | undefined;
         access_token: string | undefined;
         refresh_token: string | undefined;
     }>({
-        user_uuid: cookieHelper.getCookie('user_uuid'),
         access_token: cookieHelper.getCookie('access_token'),
         refresh_token: cookieHelper.getCookie('refresh_token'),
     });
     const [, setLoading] = useState(false);
 
-    const login = useCallback(async () => {
-        try {
-            setLoading(true);
-            const { access_token, refresh_token } = await authService.login(
-                authData.user_uuid!
-            );
-            setAuthData({ ...authData, access_token, refresh_token });
-            cookieHelper.setCookie('access_token', access_token, 1);
-            cookieHelper.setCookie('refresh_token', refresh_token, 1);
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
-    }, [authData]);
+    const login = useCallback(
+        async (user_uuid: string) => {
+            try {
+                setLoading(true);
+                const { access_token, refresh_token } = await authService.login(
+                    user_uuid
+                );
+                setAuthData({ ...authData, access_token, refresh_token });
+                cookieHelper.setCookie('access_token', access_token, 1);
+                cookieHelper.setCookie('refresh_token', refresh_token, 1);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        },
+        [authData]
+    );
 
     useEffect(() => {
-        const user_uuid = cookieHelper.getCookie('user_uuid');
+        let user_uuid = cookieHelper.getCookie('user_uuid');
 
         if (!user_uuid) {
             //generate uuid
             cookieHelper.setCookie('user_uuid', uuidv4(), 365000);
+            user_uuid = cookieHelper.getCookie('user_uuid');
+            console.log('-> user_uuid', user_uuid);
         }
+        console.log('-> user_uuid', user_uuid);
 
         if (!authData.access_token || !authData.refresh_token) {
             // void is used to ignore the return value of login() since we don't need it
-            // eslint-disable-next-line no-void
-            void login();
+            console.log(authData);
+            if (user_uuid) {
+                // eslint-disable-next-line no-void
+                void login(user_uuid);
+            } else {
+                console.log('no user_uuid');
+            }
         } else {
             verifyToken(authData.access_token).then((verified) => {
                 if (
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,7 +3,7 @@ import CardStack from '../components/CardStack';
 import { Group, Recipe, SwipeSession } from '../types';
 import recipeService from '../services/RecipeService';
 import { SessionWebsocketContext } from '../contexts/SessionContext';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import tw from '../lib/tailwind';
 import userService from '../services/UserService';
 import groupService from '../services/GroupService';
@@ -45,7 +45,15 @@ export default function HomeScreen() {
 
     return (
         <View style={tw`w-full h-full overflow-hidden`}>
-            <CardStack recipes={data} />
+            {data.length === 0 ? (
+                <View style={tw`flex-1 items-center justify-center`}>
+                    <Text style={tw`text-2xl text-gray-500`}>
+                        Recepten worden geladen...
+                    </Text>
+                </View>
+            ) : (
+                <CardStack recipes={data} />
+            )}
         </View>
     );
 }
