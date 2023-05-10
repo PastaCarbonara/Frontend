b0b758e401260953c4a5d862a68209461a80d583
mitchellvdhut <mitchellvdhut@gmail.com>
2023-05-11 00:48:24 +0200

    Added loading state, added error handling

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,27 +2,43 @@ import React, { useEffect, useState } from 'react';
 import CardStack from '../components/CardStack';
 import { Recipe } from '../types';
 import recipeService from '../services/RecipeService';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Text, View } from 'react-native';
 import tw from '../lib/tailwind';
 
 export default function HomeScreen() {
-    const [data, setData] = useState<Recipe[]>([]);
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        recipeService.fetchRecipes().then((recipes) => {
-            setData(recipes);
-        });
+        const fetchData = async () => {
+            try {
+                const fetchedRecipes = await recipeService.fetchRecipes();
+                setRecipes(fetchedRecipes);
+            } catch (error) {
+                console.log('Failed to fetch recipes:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        void fetchData();
     }, []);
     return (
         <View style={tw`w-full h-full overflow-hidden`}>
-            {data.length === 0 ? (
+            {isLoading ? (
+                <View style={tw`flex-1 items-center justify-center`}>
+                    <ActivityIndicator size="large" color="gray" />
+                    <Text style={tw`text-2xl mt-2 text-gray-500`}>
+                        Gerechten worden geladen...
+                    </Text>
+                </View>
+            ) : recipes.length === 0 ? (
                 <View style={tw`flex-1 items-center justify-center`}>
                     <Text style={tw`text-2xl text-gray-500`}>
-                        Recepten worden geladen...
+                        Geen recepten gevonden
                     </Text>
                 </View>
             ) : (
-                <CardStack recipes={data} />
+                <CardStack recipes={recipes} />
             )}
         </View>
     );
