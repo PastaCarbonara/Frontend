d9d2a577cd5167c691485dad134ae3e6914e32cf
mitchellvdhut <mitchellvdhut@gmail.com>
2023-05-11 12:55:36 +0200

    Added loading state for MyGroupsScreen.tsx, moved child components directly into the page component

--- screens/MyGroupsScreen.tsx
+++ screens/MyGroupsScreen.tsx
@@ -1,15 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
-import { Group } from '../types';
-import MyGroups from '../components/Groups';
+import {
+    ActivityIndicator,
+    ImageBackground,
+    ScrollView,
+    View,
+} from 'react-native';
+import { Group, RootStackParamList } from '../types';
 import groupService from '../services/GroupService';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import tw from '../lib/tailwind';
+import NoGroupsFound from '../components/Groups/NoGroupsFound';
+import GroupsFound from '../components/Groups/GroupsFound';
+import FloatingActionButton from '../components/FloatingActionButton';
 
 export default function MyGroupsScreen() {
-    const [data, setData] = useState<Group[] | null>(null);
+    const [myGroups, setMyGroups] = useState<Group[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
-        groupService.fetchGroups().then((groups) => {
-            setData(groups);
-        });
+        const fetchData = async () => {
+            try {
+                const groups = await groupService.fetchGroups();
+                setMyGroups(groups);
+            } catch (error) {
+                console.error('Failed to fetch groups:', error);
+                // Handle the error (e.g., display an error message)
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        void fetchData();
     }, []);
-    return data ? <MyGroups myGroups={data} /> : <Text>Loading...</Text>;
+    const navigation =
+        useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+    return (
+        <View style={tw`bg-bg_color h-full`}>
+            <ImageBackground
+                style={tw`w-full self-center grow`}
+                imageStyle={tw`w-full h-[231px] object-cover`}
+                source={require('../assets/images/header_background.svg')}
+            />
+            <ScrollView
+                style={tw`h-full mt-15`}
+                contentContainerStyle={tw`h-full`}
+            >
+                {isLoading ? (
+                    <View style={tw`flex-1 grow items-center justify-center`}>
+                        <ActivityIndicator size="large" color="gray" />
+                    </View>
+                ) : myGroups?.length < 1 ? (
+                    <NoGroupsFound />
+                ) : (
+                    <GroupsFound groups={myGroups} />
+                )}
+            </ScrollView>
+
+            <FloatingActionButton
+                icon={'plus'}
+                color={'indigo_primary'}
+                onPress={() => {
+                    navigation.navigate('CreateGroup');
+                }}
+            />
+        </View>
+    );
 }
