0ee3270f7f6981740893c82f3715d2f43d7d89fa
Marisa <60745066+07Marisa99@users.noreply.github.com>
2023-06-02 09:32:46 +0200

    deleteCookies now uses deleteCookie
    setting the value is necessary or else it sets the expiry date as the value.
    
    the value is now always set to 0 though

--- components/BottomSheetComponent.tsx
+++ components/BottomSheetComponent.tsx
@@ -23,7 +23,9 @@ export default function BottomSheetComponent({
             onBackdropPress={onBackdropPress}
         >
             <View style={tw`h-120 w-full bg-bg_color rounded-t-lg pt-5`}>
-                <Text style={tw`text-xl text-text_primary mb-6 px-3`}>
+                <Text
+                    style={tw`text-xl text-text_primary mb-6 px-3 font-Poppins-Bold text-center`}
+                >
                     {title}
                 </Text>
                 <ScrollView style={tw`px-3`}>{children}</ScrollView>
--- helpers/CookieHelper.ts
+++ helpers/CookieHelper.ts
@@ -16,29 +16,14 @@ const setCookie = (name: string, value: string, days: number) => {
 };
 
 const deleteCookie = (cookieName: string) => {
-    const cookieValue = cookieHelper.getCookie(cookieName);
-    if (cookieValue) {
-        setCookie(cookieName, cookieValue, -1);
-    }
+    const expires = new Date(Date.now() - 864e5).toUTCString();
+    document.cookie = `${cookieName}=0 expires=${expires}; path=/`;
 };
 const deleteAllCookies = () => {
-    const user_uuid = cookieHelper.getCookie('user_uuid');
-    const refresh_token = cookieHelper.getCookie('refresh_token');
-    const access_token = cookieHelper.getCookie('access_token');
-    const currentGroup = cookieHelper.getCookie('currentGroup');
-
-    if (user_uuid) {
-        cookieHelper.setCookie('user_uuid', user_uuid, -1);
-    }
-    if (refresh_token) {
-        cookieHelper.setCookie('refresh_token', refresh_token, -1);
-    }
-    if (access_token) {
-        cookieHelper.setCookie('access_token', access_token, -1);
-    }
-    if (currentGroup) {
-        cookieHelper.setCookie('currentGroup', currentGroup, -1);
-    }
+    deleteCookie('user_uuid');
+    deleteCookie('access_token');
+    deleteCookie('refresh_token');
+    deleteCookie('currentGroup');
 };
 
 export const cookieHelper = {
