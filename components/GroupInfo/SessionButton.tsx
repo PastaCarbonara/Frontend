f7ab36a876c510f317ec151df1ddce66e0464f9f
mitchellvdhut <mitchellvdhut@gmail.com>
2023-06-06 12:03:54 +0200

    now using a switch case

--- components/GroupInfo/SessionButton.tsx
+++ components/GroupInfo/SessionButton.tsx
@@ -10,67 +10,82 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
+const isSessionInThePast = (session: SwipeSession) => {
+    return (
+        new Date(session.session_date) <= new Date(new Date().toDateString())
+    );
+};
+
 export default function SessionButton({ session }: { session: SwipeSession }) {
+    //write a switch case to render a different button depending on the status of the session
+    const sessionButtonSwitch = () => {
+        switch (session.status) {
+            case SwipeSessionStatus.COMPLETED:
+                return <ViewSessionButton session={session} />;
+            case SwipeSessionStatus.READY:
+                return <StartSessionButton session={session} />;
+            case SwipeSessionStatus.PAUSED:
+                return <StartSessionButton session={session} />;
+            case SwipeSessionStatus.IN_PROGRESS:
+                return <StopSessionButton session={session} />;
+            default:
+                return <></>;
+        }
+    };
+
+    return <>{sessionButtonSwitch()}</>;
+}
+
+function StartSessionButton({ session }: { session: SwipeSession }) {
     return (
         <>
-            {session.status === SwipeSessionStatus.COMPLETED ? (
-                <ViewSessionButton session={session} />
-            ) : new Date(session.session_date) <=
-              new Date(new Date().toDateString()) ? (
-                <></>
-            ) : session.status === SwipeSessionStatus.READY ||
-              session.status === SwipeSessionStatus.PAUSED ? (
-                <StartSessionButton session={session} />
-            ) : session.status === SwipeSessionStatus.IN_PROGRESS ? (
-                <StopSessionButton session={session} />
-            ) : (
-                <></>
+            {!isSessionInThePast(session) && (
+                <Pressable
+                    style={tw`items-center justify-center p-4 gap-4 h-9 bg-orange_primary rounded-lg `}
+                    onPress={async () => {
+                        console.log('Sessie wordt gestart');
+                        await swipeSessionService.updateSwipeSessionStatus({
+                            groupId: session.group_id,
+                            swipeSessionId: session.id,
+                            status: SwipeSessionStatus.IN_PROGRESS,
+                        });
+                    }}
+                >
+                    <Text
+                        style={tw`font-sans font-bold text-base leading-normal text-white`}
+                    >
+                        Start
+                    </Text>
+                </Pressable>
             )}
         </>
     );
 }
 
-function StartSessionButton({ session }: { session: SwipeSession }) {
-    return (
-        <Pressable
-            style={tw`items-center justify-center p-4 gap-4 h-9 bg-orange_primary rounded-lg `}
-            onPress={async () => {
-                console.log('Sessie wordt gestart');
-                await swipeSessionService.updateSwipeSessionStatus({
-                    groupId: session.group_id,
-                    swipeSessionId: session.id,
-                    status: SwipeSessionStatus.IN_PROGRESS,
-                });
-            }}
-        >
-            <Text
-                style={tw`font-sans font-bold text-base leading-normal text-white`}
-            >
-                Start
-            </Text>
-        </Pressable>
-    );
-}
-
 function StopSessionButton({ session }: { session: SwipeSession }) {
     return (
-        <Pressable
-            style={tw`items-center justify-center p-4 gap-4 h-9 bg-orange_primary rounded-lg `}
-            onPress={async () => {
-                console.log('Stop');
-                await swipeSessionService.updateSwipeSessionStatus({
-                    groupId: session.group_id,
-                    swipeSessionId: session.id,
-                    status: SwipeSessionStatus.CANCELLED,
-                });
-            }}
-        >
-            <Text
-                style={tw`font-sans font-bold text-base leading-normal text-white`}
-            >
-                Stop
-            </Text>
-        </Pressable>
+        <>
+            {' '}
+            {!isSessionInThePast(session) && (
+                <Pressable
+                    style={tw`items-center justify-center p-4 gap-4 h-9 bg-orange_primary rounded-lg `}
+                    onPress={async () => {
+                        console.log('Stop');
+                        await swipeSessionService.updateSwipeSessionStatus({
+                            groupId: session.group_id,
+                            swipeSessionId: session.id,
+                            status: SwipeSessionStatus.CANCELLED,
+                        });
+                    }}
+                >
+                    <Text
+                        style={tw`font-sans font-bold text-base leading-normal text-white`}
+                    >
+                        Stop
+                    </Text>
+                </Pressable>
+            )}
+        </>
     );
 }
 
